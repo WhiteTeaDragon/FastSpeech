298d323ed9112a940f78c97eff770886483868b9
asendero <AlexSend57@gmail.com>
2021-12-02 20:41:49 +0300

    move duration to gpu

--- fastspeech/trainer/trainer.py
+++ fastspeech/trainer/trainer.py
@@ -73,7 +73,7 @@ class Trainer(BaseTrainer):
         Move all necessary tensors to the HPU
         """
         for tensor_for_gpu in ["audio", "audio_length", "text_encoded",
-                               "token_lengths", "melspec"]:
+                               "token_lengths", "melspec", "duration"]:
             batch[tensor_for_gpu] = batch[tensor_for_gpu].to(device)
         return batch
 
