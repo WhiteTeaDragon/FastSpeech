0ca8b3439515b5ef01431310580f9624889f48a5
asendero <AlexSend57@gmail.com>
2021-12-04 18:08:16 +0300

    add alpha to loss + new config

--- fastspeech/loss/FastSpeechLoss.py
+++ fastspeech/loss/FastSpeechLoss.py
@@ -2,8 +2,9 @@ import torch
 
 
 class FastSpeechLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, alpha):
         super().__init__()
+        self.alpha = alpha
 
     def forward(self, *args, **kwargs) -> dict:
         slice_until = min(kwargs["output_duration"].shape[-1],
@@ -20,5 +21,5 @@ class FastSpeechLoss(torch.nn.Module):
         true_term = (kwargs["melspec"][..., :slice_until].transpose(0, 1) *
                      mask[..., :slice_until]).transpose(0, 1)
         melspec_loss = torch.nn.functional.mse_loss(output_term, true_term)
-        return {"loss": duration_loss + melspec_loss,
+        return {"loss": duration_loss + self.alpha * melspec_loss,
                 "duration_loss": duration_loss, "melspec_loss": melspec_loss}
