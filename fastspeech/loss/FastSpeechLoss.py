2bd232d0a680d8e2b54c8f9c64e452c81766a48e
asendero <AlexSend57@gmail.com>
2021-12-05 23:13:49 +0300

    add both log and exp to loss

--- fastspeech/loss/FastSpeechLoss.py
+++ fastspeech/loss/FastSpeechLoss.py
@@ -2,20 +2,28 @@ import torch
 
 
 class FastSpeechLoss(torch.nn.Module):
-    def __init__(self, alpha):
+    def __init__(self, alpha, mode):
         super().__init__()
         self.alpha = alpha
+        assert mode in ["log", "exp"]
+        self.mode = mode
 
     def forward(self, *args, **kwargs) -> dict:
         slice_until = min(kwargs["output_duration"].shape[-1],
                           kwargs["duration"].shape[-1])
-        zero_mask = torch.isclose(kwargs["duration"][..., :slice_until].float(),
-                                  torch.tensor(0).float())
-        log_durations = torch.log(kwargs["duration"][..., :slice_until])
-        log_durations[zero_mask] = -7
-        duration_loss = torch.nn.functional.mse_loss(kwargs["output_duration"][
-                                                     ..., :slice_until],
-                                                     log_durations)
+        if self.mode == "log":
+            zero_mask = torch.isclose(kwargs["duration"][..., :slice_until].
+                                      float(), torch.tensor(0).float())
+            log_durations = torch.log(kwargs["duration"][..., :slice_until])
+            log_durations[zero_mask] = -7
+            duration_loss = torch.nn.functional.mse_loss(kwargs[
+                                                             "output_duration"]
+                                                         [..., :slice_until],
+                                                         log_durations)
+        else:
+            duration_loss = torch.nn.functional.mse_loss(torch.exp(
+                kwargs["output_duration"][..., :slice_until]),
+                kwargs["duration"][..., :slice_until].float())
         slice_until = min(kwargs["output_melspec"].shape[-1],
                           kwargs["melspec"].shape[-1])
         mask = ~kwargs["output_mask"].detach()
